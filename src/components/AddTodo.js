ad57521cda77fb4e52977761a85c9dc16ee88564
Branko Petrevski <bpetrevski2@gmail.com>
2024-10-06 23:49:59 +0200

    redux

---
+++ src/components/AddTodo.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../app/features/todo/todoslice'
+
+export default function AddTodo() {
+    const [input, setInput] = useState('')
+
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) =>  {
+        e.preventDefault()
+
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+    return (
+        <form onSubmit={addTodoHandler}>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+            <button type="submit">Add Todo</button>
+        </form>
+        // <div>AddTodo</div>
+    )
+}
